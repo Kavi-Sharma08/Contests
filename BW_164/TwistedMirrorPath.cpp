e8796016fbc591b8a00c60e7cfb9990a42c2bcbe
Kavi <thekavisharma26@gmail.com>
2025-09-05 00:24:57 +0530

    In ques 2 change the return value from void to int

--- BW_164/TwistedMirrorPath.cpp
+++ BW_164/TwistedMirrorPath.cpp
@@ -2,33 +2,31 @@
 using namespace std;
 
 // Recursive Code
-void helper(vector<vector<int>> &grid , int m , int n , int i , int j , int &count , int dir){
+int helper(vector<vector<int>> &grid , int m , int n , int i , int j , int &count , int dir){
     if(i == m-1 && j == n-1){
-        count++;
-        return;
+        return 1;
     }
     if(i >= m || j >= n){
-        return ;
+        return 0;
     }
+    int right = 0 ;
+    int down = 0;
     if(grid[i][j]==1){
         if(dir == 0){
-            helper(grid , m , n , i+1 , j , count , 1 );
+            right = helper(grid , m , n , i+1 , j , count , 1 );
 
         }
         else if(dir == 1){
-            helper(grid , m , n , i , j+1 , count , 0);
+            down = helper(grid , m , n , i , j+1 , count , 0);
         }
     }
     else{
         
-        helper(grid , m , n , i , j+1 , count , 0);
-        helper(grid , m , n , i+1 , j , count , 1);
+        right = helper(grid , m , n , i , j+1 , count , 0);
+        down = helper(grid , m , n , i+1 , j , count , 1);
 
     }
-
-    
-   
-    
+    return (right+down);
 
 }
 int main(){
@@ -40,7 +38,6 @@ int main(){
     int n = grid[0].size();
     int count = 0 ;
     int dir = -1;
-    helper(grid , m , n , 0 , 0 , count , dir);
-    cout<<count;
+    cout<<helper(grid , m , n , 0 , 0 , count , dir);
 
 }
